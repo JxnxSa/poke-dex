e1c1acf9dc432d4eb188971e2e3fb63d5a0707a3
Sxrxwxlxx <tangjittham_s@silpakorn.edu>
2024-03-18 21:46:47 +0700

    change color

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import axios from "axios";
 function App() {
   const [offset, setOffset] = useState(0);
   let url = "https://pokeapi.co/api/v2/pokemon";
+  const limit = 24;
 
   const [pokemonList, setPokemonList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const response = await axios.get(url, {
-        params: { offset: offset, limit: 20 },
+        params: { offset: offset, limit: limit },
       });
       const data = response.data;
 
@@ -22,34 +25,41 @@ function App() {
 
       const pokemonDetails = await Promise.all(promises);
       setPokemonList(pokemonDetails);
+      setLoading(false);
     };
 
     fetchData();
-    // console.log(pokemonList);
   }, [offset]);
 
-
   const prevPage = () => {
-    setOffset((offset) => offset - 20);
-    console.log(offset);
+    setOffset((offset) => offset - limit);
   };
 
   const nextPage = () => {
-    setOffset((offset) => offset + 20);
-    console.log(offset);
+    setOffset((offset) => offset + limit);
   };
 
   return (
     <>
-      <div className="container ">
+      <div className="container p-3">
         <h1 style={{ textAlign: "center" }}>Pokemon Dex</h1>
-        <div className="row">
-          {pokemonList.map((pokemon, index) => (
-            <div key={index} className="col-sm12 col-md-4 col-lg-3">
-              <Card pokemon={pokemon} />
+
+        {loading ? (
+          <div className="d-flex justify-content-center">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
             </div>
-          ))}
-        </div>
+          </div>
+        ) : (
+          <div className="row">
+            {pokemonList.map((pokemon, index) => (
+              <div key={index} className="col-sm12 col-md-4 col-lg-3">
+                <Card pokemon={pokemon} />
+              </div>
+            ))}
+          </div>
+        )}
+
         <div className="row">
           <div className="col text-left">
             {offset !== 0 && (
@@ -58,7 +68,12 @@ function App() {
               </button>
             )}
           </div>
-          <div className="col d-flex justify-content-center" style={{ fontSize: "1.5rem" }}>{offset / 20 + 1}</div>
+          <div
+            className="col d-flex justify-content-center"
+            style={{ fontSize: "1.5rem" }}
+          >
+            Page : {offset / limit + 1}
+          </div>
           <div className="col d-flex justify-content-end">
             <button className="btn btn-primary" onClick={nextPage}>
               Next
